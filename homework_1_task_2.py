dac70933b9a094358becdbeb2673c9fede632e1a
Anna <ds_anna@bk.ru>
2022-07-10 20:12:25 +0300

    ¬(X V Y V Z) = ¬X Л ¬Y Л ¬Z

--- homework_1_task_2.py
+++ homework_1_task_2.py
@@ -2,10 +2,7 @@
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
 for x in range(2):
-    print(x,end=' ')
-for y in range(2):
-    print( y,end=' ')
-for z in range(2):
-    print( z,end=' ')
-
-print(not (x or y or z) == (not x and not y and not z))
+    for y in range(2):
+        for z in range(2):
+            print(x, y, z)
+            print(not (x or y or z) == (not x and not y and not z))
