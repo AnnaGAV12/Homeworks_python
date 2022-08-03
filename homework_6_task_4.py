b2aae42aa96167d32c09aa08140735789669a868
Anna <ds_anna@bk.ru>
2022-08-03 22:27:16 +0300

    сумму цифр вещественного числа

---
+++ homework_6_task_4.py
@@ -0,0 +1,16 @@
+# Напишите программу, которая принимает на вход вещественное число
+# и показывает сумму его цифр
+# Пример: - 6782 -> 23    - 0,56 -> 11
+
+# def digit(number: float):
+#     digits_string = str(number)
+#     sum = 0
+#     for num in digits_string:
+#         if num != '.':
+#             sum += int(num)
+#     return sum
+
+# num = float(input('Введите число: '))
+# print(f'Сумма цифр введенного числа равна {digit(num)}')
+
+print(f'Сумма чисел равна {sum(list(map(lambda x: int(x) ,list(filter(lambda x: x.isdigit(), input("Введите вещественное число: "))))))}')
